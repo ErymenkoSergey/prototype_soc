735f9275fc358ec6b0118cadf5dacd8dc25fe7c6
Eryomenko Sergey <eryomenkosergey10@gmail.com>
2025-12-23 12:52:21 +0700

    3

---
+++ Assets/ThunderWire Studio/UHFPS/Content/Scripts/Runtime/RenderFeatures/EffectFeature.cs
@@ -0,0 +1,25 @@
+using UnityEngine.Rendering.Universal;
+using System;
+using UnityEngine.Rendering.HighDefinition;
+
+namespace UHFPS.Rendering
+{
+    [Serializable]
+    public abstract class EffectFeature
+    {
+        public ScriptableRenderPass RenderPass;
+        public bool Enabled = true;
+
+        public abstract string Name { get; }
+        public abstract void OnCreate();
+
+        public virtual ScriptableRenderPass OnGetRenderPass()
+        {
+            if (RenderPass == null)
+                return null;
+
+            RenderPass.ConfigureInput(ScriptableRenderPassInput.Color);
+            return RenderPass;
+        }
+    }
+}
